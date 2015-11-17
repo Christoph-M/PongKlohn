6796ceabdc6ffd7f4e20ba7c4ae9107a4bc38b81
Christoph-M <Christoph.Marschott@games-academy.de>
2015-11-17 12:11:11 +0100

    Moved GameMechanics/PlayerInput/Trigger into Game/Player/Ball

--- PongKlohn/Assets/Scripts/Player.cs
+++ PongKlohn/Assets/Scripts/Player.cs
@@ -1,66 +1,162 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 	//public GameObject shootLeft; 
 	//public GameObject shootForward; 
 	//public GameObject shootRight;
 	//public GameObject shootSuppi;
-	
-	
-	private Rigidbody2D myTransform;
-	private Animator animator;
-	
-	private string xAxis = "x";
-	private string yAxis = "y";
+	public GameObject sphereO;
+	public Transform catchTriggerO;
+	public Transform blockTriggerO;
 	
 	public float speed = 10;
 	public bool InvertMotion = false;
+	
+	private Rigidbody2D myTransform;
+	private Animator animator;
+	private Ball projectile;
+
+	private GameObject sphereC;
+	private Transform catchTriggerC;
+	private Transform blockTriggerC;
+	
+	private string xAxis = "x";
+	private string yAxis = "y";
+	private string shoot = "s";
+	private string block = "b";
+	private string dash  = "d";
+
+	private bool turn;
+
 	private float motionInverter = 1;
 	
-	void Start() 
-	{
+	void Start() {
 		animator = GetComponent<Animator>();
 		myTransform = this.GetComponent<Rigidbody2D>();
-		if(InvertMotion)
-		{
+
+		catchTriggerC = Instantiate (catchTriggerO, this.transform.position, this.transform.rotation) as Transform;
+		catchTriggerC.parent = this.transform;
+		catchTriggerC.name = "Catch_Trigger";
+		
+		blockTriggerC = Instantiate (blockTriggerO, this.transform.position, this.transform.rotation) as Transform;
+		blockTriggerC.gameObject.SetActive (false);
+		blockTriggerC.parent = this.transform;
+		blockTriggerC.name = "Block_Trigger";
+
+		if(InvertMotion) {
 			motionInverter = -1;
-		}
-		else
-		{
+		} else {
 			motionInverter = 1;
 		}
 	}
 	
-	void Update()
-	{
+	void FixedUpdate() {
 		Vector2 direction = new Vector2(Input.GetAxis(xAxis),Input.GetAxis(yAxis));
-		myTransform.AddForce (direction * speed);
-		Debug.Log(direction);
-		animator.SetFloat("xAxis",direction.x * motionInverter);
-		animator.SetFloat("yAxis",direction.y * motionInverter);
+		float shooting = Input.GetAxisRaw (shoot);
+		float blocking = Input.GetAxisRaw (block);
+
+		if (blocking == 1.0f) {
+			blockTriggerC.gameObject.SetActive (true);
+			
+			animator.SetFloat("xAxis", 0.0f);
+			animator.SetFloat("yAxis", 0.0f);
+		} else {
+			blockTriggerC.gameObject.SetActive (false);
+
+			myTransform.AddForce (direction * Game.playerSpeed);
+			
+			animator.SetFloat("xAxis", direction.x * motionInverter);
+			animator.SetFloat("yAxis", direction.y * motionInverter);
+		}
+
+		if (shooting == 1.0f && turn) {
+			if (this.name == "Player_01"){
+				p1Shoot();
+			} else {
+				p2Shoot();
+			}
+		}
+	}
+
+	void Update(){
+		catchTriggerC.position = this.transform.position;
+		blockTriggerC.position = this.transform.position;
 	}
 	
-	
-	public void SetInputAxis(string x,string y)
-	{
+	public void SetInputAxis(string x,string y, string s, string b) {
 		xAxis = x;
 		yAxis = y;
+		shoot = s;
+		block = b;
+	}
+
+	public bool setTurn(bool turnt){
+		return turn = turnt;
+	}
+
+	public void instantiateSphere(GameObject sphereO, Vector3 position, Quaternion rotation){
+		sphereC = Instantiate(sphereO, position, rotation) as GameObject;
+		
+		sphereC.name = "Projectile";
+		projectile = new Ball(sphereC.transform);
 	}
 	
-	public Vector3 GetProjectilePositin() 
-	{
+	public void p1Shoot(){
+		float ver = Input.GetAxisRaw("VerticalP1");
+		
+		Quaternion angle = Quaternion.identity;
+		angle = this.transform.rotation;
+		
+		if (!sphereC){
+			if(ver > 0){
+				angle=Quaternion.AngleAxis(45.0f, Vector3.forward);
+			}
+			
+			if(ver < 0){
+				angle=Quaternion.AngleAxis(-45.0f, Vector3.forward);
+			}
+			
+			instantiateSphere(sphereO, (Vector2)this.transform.position + new Vector2(Game.ballSpawnDistance, 0.0f), angle);
+			//sphereC.AddForce (sphereC.gameObject.transform.TransformVector(Vector2.right) * ballSpeed, ForceMode2D.Impulse);
+		}
+	}
+	
+	public void p2Shoot(){
+		float ver = Input.GetAxisRaw ("VerticalP2");
+		
+		Quaternion angle = Quaternion.identity;
+		angle = this.transform.rotation;
+		
+		if (!sphereC) {
+			if (ver > 0) {
+				angle = Quaternion.AngleAxis (135.0f, Vector3.forward);
+			}
+			
+			if (ver < 0) {
+				angle = Quaternion.AngleAxis (225.0f, Vector3.forward);
+			}
+			
+			instantiateSphere (sphereO, (Vector2)this.transform.position + new Vector2 (-Game.ballSpawnDistance, 0.0f), angle);
+			//sphereC.AddForce (sphereC.gameObject.transform.TransformVector(Vector2.right) * ballSpeed, ForceMode2D.Impulse);
+		}
+	}
+	
+	public Vector3 GetProjectilePositin() {
 		return myTransform.gameObject.transform.position;
 	}
 	
-	public Quaternion Rotation() 
-	{
+	public Quaternion Rotation() {
 		return myTransform.gameObject.transform.rotation;
 	}
 	
-	public Vector3 GetRotation()
-	{
+	public Vector3 GetRotation(){
 		return new Vector3(myTransform.gameObject.transform.rotation.x, myTransform.gameObject.transform.rotation.y, myTransform.gameObject.transform.rotation.z);
+	}
+
+	public void moveBall(){
+		if (sphereC) {
+			projectile.move (Game.ballSpeed);
+		}
 	}
 }
--- PongKlohn/Assets/Scripts/Trigger/Block_Trigger.cs
+++ PongKlohn/Assets/Scripts/Trigger/Block_Trigger.cs
@@ -3,57 +3,55 @@ using System.Collections;
 
 public class Block_Trigger : MonoBehaviour {
 	private Game goal;
-	private GameMechanics gameMechanics;
 	
 	// Use this for initialization
 	void Start () {
 		goal = GameObject.FindObjectOfType (typeof(Game)) as Game;
-		gameMechanics = GameObject.FindObjectOfType (typeof(GameMechanics)) as GameMechanics;
 	}
 	
 	void OnTriggerStay2D(Collider2D other) {
-		if (other.name == "Projectile") {
-			if (this.name == "Block_Trigger_Player_01") {
-				float lShift = Input.GetAxisRaw ("BlockP1");
-
-				Vector2 playerDirection = gameMechanics.figure1.transform.position - other.transform.position;
-				
-				RaycastHit2D hit = Physics2D.Raycast(other.transform.position, playerDirection);
-				Debug.DrawRay(other.transform.position, playerDirection, Color.green, 20);
-				Debug.Log("Hit object: " + hit.collider.gameObject.name);
-
-				if (lShift > 0) {
-					Vector2 position = other.transform.position;
-					Vector2 direction = Vector2.Reflect (gameMechanics.figure1.transform.right, hit.normal);
-					float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-					
-					Object.Destroy (other.gameObject);
-					
-					Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-					
-					gameMechanics.instantiateSphere(gameMechanics.sphereRO, position, rotation);
-				}
-			} else {
-				float rShift = Input.GetAxisRaw ("BlockP2");
-
-				Vector2 playerDirection = gameMechanics.figure2.transform.position - other.transform.position;
-				
-				RaycastHit2D hit = Physics2D.Raycast(other.transform.position, other.transform.TransformDirection(playerDirection));
-				Debug.DrawRay(other.transform.position, playerDirection, Color.green, 20);
-				Debug.Log("Hit object: " + hit.collider.gameObject.name);
-
-				if (rShift > 0){					
-					Vector2 position = other.transform.position;
-					Vector2 direction = Vector2.Reflect (gameMechanics.figure2.transform.right, hit.normal);
-					float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-					Object.Destroy (other.gameObject);
-					
-					Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-					
-					gameMechanics.instantiateSphere(gameMechanics.sphereBO, position, rotation);
-				}
-			}
-		}
+//		if (other.name == "Projectile") {
+//			if (this.name == "Block_Trigger_Player_01") {
+//				float lShift = Input.GetAxisRaw ("BlockP1");
+//
+//				Vector2 playerDirection = gameMechanics.figure1.transform.position - other.transform.position;
+//				
+//				RaycastHit2D hit = Physics2D.Raycast(other.transform.position, playerDirection);
+//				Debug.DrawRay(other.transform.position, playerDirection, Color.green, 20);
+//				Debug.Log("Hit object: " + hit.collider.gameObject.name);
+//
+//				if (lShift > 0) {
+//					Vector2 position = other.transform.position;
+//					Vector2 direction = Vector2.Reflect (gameMechanics.figure1.transform.right, hit.normal);
+//					float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+//					
+//					Object.Destroy (other.gameObject);
+//					
+//					Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+//					
+//					gameMechanics.instantiateSphere(gameMechanics.sphereRO, position, rotation);
+//				}
+//			} else {
+//				float rShift = Input.GetAxisRaw ("BlockP2");
+//
+//				Vector2 playerDirection = gameMechanics.figure2.transform.position - other.transform.position;
+//				
+//				RaycastHit2D hit = Physics2D.Raycast(other.transform.position, other.transform.TransformDirection(playerDirection));
+//				Debug.DrawRay(other.transform.position, playerDirection, Color.green, 20);
+//				Debug.Log("Hit object: " + hit.collider.gameObject.name);
+//
+//				if (rShift > 0){					
+//					Vector2 position = other.transform.position;
+//					Vector2 direction = Vector2.Reflect (gameMechanics.figure2.transform.right, hit.normal);
+//					float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+//
+//					Object.Destroy (other.gameObject);
+//					
+//					Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+//					
+//					gameMechanics.instantiateSphere(gameMechanics.sphereBO, position, rotation);
+//				}
+//			}
+//		}
 	}
 }

f30af10759a445d1e4bdc9db7ecae6af17fc7ac0
DrBanane <albrecht.kiesewetter@gmx.de>
2015-11-30 19:09:09 +0100

    Modified player trigger

--- PongKlohn/Assets/Scripts/Player.cs
+++ PongKlohn/Assets/Scripts/Player.cs
@@ -15,16 +15,18 @@ public class Player : MonoBehaviour {
 	public bool InvertMotion = false;
 	
 	private Timer catchTimer;
+	private Timer blockTimer;
+	private Timer fireTimer;
 
 	private Rigidbody2D myTransform;
 	private Animator animator;
-	private Ball projectile;
 	
 	private GameObject ball;
 
 	private GameObject catchTrigger;
 	private GameObject blockTrigger;
 	private GameObject dashTrigger;
+	private GameObject missTrigger;
 		
 	private string xAxis = "x";
 	private string yAxis = "y";
@@ -39,111 +41,212 @@ public class Player : MonoBehaviour {
 	void Start() 
 	{
 		catchTimer = new Timer();
+		blockTimer  = new Timer();
+		fireTimer = new Timer();
+		
 		animator = GetComponent<Animator>();
 		myTransform = this.GetComponent<Rigidbody2D>();
-
-		catchTrigger = Instantiate(catchCollider, transform.position, transform.rotation) as GameObject;
-		blockTrigger = Instantiate(blockCollider, transform.position, transform.rotation) as GameObject;
-		dashTrigger = Instantiate(dashCollider, transform.position, transform.rotation) as GameObject;
-		catchTrigger.transform.parent = this.transform;
-		blockTrigger.transform.parent = this.transform;
-		dashTrigger.transform.parent = this.transform;
-		catchTrigger.SetActive(true);
-		blockTrigger.SetActive(false);
-		dashTrigger.SetActive(false);
-		catchTrigger.name = "Catch_Trigger";
-		blockTrigger.name = "Block_Trigger";
-		dashTrigger.name = "Dash_Trigger";
 		
-		if (InvertMotion) {
+		var children = taube.GetComponentsInChildren<GameObject>();// finde Trigger  
+		foreach (var child in children)
+		{
+			if (child.name == "Catch_Trigger")
+			{
+				catchTrigger = child;
+			}
+			
+			if (child.name == "Block_Trigger")
+			{
+				blockTrigger = child;
+			}
+			
+			if (child.name == "Dash_Trigger")
+			{
+				dashTrigger = child;
+			}
+			
+			if (child.name == "Miss_Trigger")
+			{
+				missTrigger = child;
+			}
+		}
+
+		if (InvertMotion) //Spieler Steht Links oder Recht   Steuerung anpassen
+		{
 			motionInverter = -1;
 		} else {
 			motionInverter = 1;
 		}
 	}
 	
+	private bool dashHasBeenTriggert = false;
+	private bool zuLangsamZumFangenDuMong = false;
 	private bool canMovement = true;
 	private bool axisInUse = false;
 	private bool fired = false;
 	private float timeLeft = 1.0f;
+	
+	private bool isStunned = false;
+	private bool onFireKeyDown = true;
+	private bool onBlockKeyDown = true;
+	
 	void Update() 
 	{
-		Vector2 direction = new Vector2(Input.GetAxis(xAxis), Input.GetAxis(yAxis));
-		Vector2 directionRaw = new Vector2(Input.GetAxisRaw(xAxis), Input.GetAxisRaw(yAxis));
-
-		if (Input.GetAxis(shoot) != 0f)
+		/////////////////INPUT
+		
+		if(isStunned)
 		{
-			animator.SetBool("Fire", true);
-			canMovement = false;
-		}
-		else
-		{
-			canMovement = true;
-			animator.SetBool("Fire", false);
-		}
-
-		if (Input.GetAxisRaw (block) != 0f && (directionRaw.x == 0f && directionRaw.y == 0f)) {
-			canMovement = false;
-			animator.SetBool ("Block", true);
-			blockTrigger.SetActive (true);
-
-			axisInUse = false;
-		} else if (Input.GetAxisRaw (block) != 0f) {
-			canMovement = false;
-
-			if (!axisInUse) {
-				animator.SetBool ("Block", false);
-				animator.SetFloat("xAxis", direction.x * motionInverter);
-				animator.SetFloat("yAxis", direction.y * motionInverter);
-				myTransform.AddForce (directionRaw * dashSpeed, ForceMode2D.Impulse);
-
-				axisInUse = true;
-			} else {
-				animator.SetBool ("Block", true);
-				blockTrigger.SetActive (true);
-			}
-		} else if (Input.GetAxisRaw (block) == 0f) {
-			animator.SetBool("Block", false);
-			blockTrigger.SetActive(false);
-			canMovement = true;
-			
-			axisInUse = false;
-		}
-
-		if (canMovement)
-		{
-			animator.SetFloat("xAxis", direction.x * motionInverter);
-			animator.SetFloat("yAxis", direction.y * motionInverter);
-			myTransform.AddForce (direction * speed);
-		}
-
-		if (Input.GetAxis (shoot) != 0f && turn && !ball) {//mitte schiessen
-			timeLeft -= Time.deltaTime;
-			fired = true;
-		} else if (fired && turn && !ball) {
-			if (timeLeft > 0) {
-				Debug.Log("Regular");
-				if (Input.GetAxisRaw (yAxis) == 1f) {//oben schiessen
-					ball = Shoot (balls [1], ballSpohorn.position, this.transform.rotation);
-				} else if (Input.GetAxisRaw (yAxis) == -1f) {//unten schiessen
-					ball = Shoot (balls [2], ballSpohorn.position, this.transform.rotation);
-				} else if (Input.GetAxisRaw (yAxis) == 0f) {
-					ball = Shoot (balls [0], ballSpohorn.position, this.transform.rotation);
-				}
-
-				timeLeft = 1.0f;
-				fired = false;
-			} else if (turn && !ball) {
-				Debug.Log("Special");
-				ball = Shoot(balls[3], ballSpohorn.position, this.transform.rotation);
-
-				timeLeft = 1.0f;
-				fired = false;
-			}
+			animator.SetBool("Stunn");
 		}
 		
-		catchTimer.Update();
-		Debug.Log (timeLeft);
+		else
+		{
+			Vector2 direction = new Vector2(Input.GetAxis(xAxis), Input.GetAxis(yAxis));//zuweisung der Inputachsen
+			Vector2 directionRaw = new Vector2(Input.GetAxisRaw(xAxis), Input.GetAxisRaw(yAxis));
+
+			if (Input.GetAxis(shoot) != 0f)//Intput   Feuerachse
+			{
+				timeLeft -= Time.deltaTime;
+				animator.SetBool("Fire", true);
+				if(onFireKeyDown && fireTimer.IsFinished())
+				{
+					fireTimer.SetTimer(1);
+				}
+				canMovement = false;
+			}
+			
+			else
+			{
+				animator.SetBool("Fire", false);
+				canMovement = true;
+			}
+			
+			if (Input.GetAxis(block) != 0f)// input Block
+			{
+				canMovement = false;
+				//if(onBlockKeyDown)
+				//{
+					blockTimer(0.5f);
+					
+				//}	
+				animator.SetBool ("Block", true);
+			}
+			else
+			{
+				canMovement = true;
+				animator.SetBool ("Block", false);
+			}
+			
+			//////////////Output
+			
+			if(fireTimer.IsFinished() && turn && !ball)//Shoot
+			{
+				fireTimer.RestTimer();
+				canMovement = false;
+				catchTrigger.SetActive(false);
+				blockTrigger.SetActive(false);
+				dashTrigger.SetActive(false);
+				
+				if(timeLeft<=0.1)
+				{
+					if(transform.position.y>3){ball = Shoot(balls[5], ballSpohorn.position, this.transform.rotation);}//Spezial oban
+					else if(transform.position.y<-3){ball = Shoot(balls[4], ballSpohorn.position, this.transform.rotation);}//Special unten
+					else{ball = Shoot(balls[3], ballSpohorn.position, this.transform.rotation);}//speschel mitte
+					//speschel schuss
+				}
+				else
+				{
+					//normal schuss
+					if (Input.GetAxisRaw (yAxis) == 1f) {//oben schiessen
+						ball = Shoot (balls [1], ballSpohorn.position, this.transform.rotation);
+					} else if (Input.GetAxisRaw (yAxis) == -1f) {//unten schiessen
+						ball = Shoot (balls [2], ballSpohorn.position, this.transform.rotation);
+					} else if (Input.GetAxisRaw (yAxis) == 0f) {//normaler schuss
+						ball = Shoot (balls [0], ballSpohorn.position, this.transform.rotation);
+					}
+				}
+			}
+			else
+			{
+				catchTrigger.SetActive(true);
+			}
+			
+			
+			//Block
+			if (blockTimer.IsFinished() == false) //Input Blocktaste
+			{
+				canMovement = false;
+				missTrigger.SetActive(true);
+				if(!zuLangsamZumFangenDuMong)
+				{
+					blockTrigger.SetActive (true);
+					missTrigger.SetActive(false);
+				}
+				catchTrigger.SetActive (true);
+				
+
+				axisInUse = false;
+			} else if (Input.GetAxisRaw (block) != 0f) {
+				canMovement = false;
+
+				if (!axisInUse) {
+					
+					animator.SetFloat("xAxis", direction.x * motionInverter);
+					animator.SetFloat("yAxis", direction.y * motionInverter);
+					myTransform.AddForce (directionRaw * dashSpeed, ForceMode2D.Impulse);
+
+					axisInUse = true;
+				} else {
+					animator.SetBool ("Block", true);
+					blockTrigger.SetActive (true);
+				}
+			} else if (Input.GetAxisRaw (block) == 0f) {
+				animator.SetBool("Block", false);
+				blockTrigger.SetActive(false);
+				canMovement = true;
+				
+				axisInUse = false;
+			}
+
+
+
+			if (Input.GetAxis (shoot) != 0f && turn && !ball) {//mitte schiessen
+				
+				fired = true;
+			} else if (fired && turn && !ball) {
+				if (timeLeft > 0) {
+					Debug.Log("Regular");
+					if (Input.GetAxisRaw (yAxis) == 1f) {//oben schiessen
+						ball = Shoot (balls [1], ballSpohorn.position, this.transform.rotation);
+					} else if (Input.GetAxisRaw (yAxis) == -1f) {//unten schiessen
+						ball = Shoot (balls [2], ballSpohorn.position, this.transform.rotation);
+					} else if (Input.GetAxisRaw (yAxis) == 0f) {
+						ball = Shoot (balls [0], ballSpohorn.position, this.transform.rotation);
+					}
+
+					timeLeft = 1.0f;
+					fired = false;
+				} else if (turn && !ball) {
+					Debug.Log("Special");
+					ball = Shoot(balls[3], ballSpohorn.position, this.transform.rotation);
+
+					timeLeft = 1.0f;
+					fired = false;
+				}
+			}
+			
+			if (canMovement)// Bewegt sen spieler
+			{
+				animator.SetFloat("xAxis", direction.x * motionInverter);
+				animator.SetFloat("yAxis", direction.y * motionInverter);
+				myTransform.AddForce (direction * speed);
+			}
+			
+			fireTimer.Update();
+			blockTimer.Update();
+			catchTimer.Update();
+			Debug.Log (timeLeft);
+		}
 	}
 
 	void FixedUpdate()
@@ -158,7 +261,17 @@ public class Player : MonoBehaviour {
 		shoot = s;
 		block = b;
 	}
-
+	
+	public void SetZuLangsamZumFangenDuMong(bool zLZFDM)
+	{
+		zuLangsamZumFangenDuMong = zLZFDM;
+	}
+	
+	public void SetDashTrigger(bool dT)
+	{
+		dashHasBeenTriggert = dt;
+	}
+	
 	public bool setTurn(bool turnt){
 		return turn = turnt;
 	}

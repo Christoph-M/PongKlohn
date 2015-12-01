16fb617198e8f7dd9638dfbd4c5fc95c5fd4db86
Christoph-M <Christoph.Marschott@games-academy.de>
2015-12-01 16:27:16 +0100

    partically implemented win/lose state + added UI element prefabs

--- PongKlohn/Assets/Scripts/Game.cs
+++ PongKlohn/Assets/Scripts/Game.cs
@@ -14,14 +14,15 @@ public class Game : MonoBehaviour {
 	public float maxBallSpeed = 100.0f;
 	public float ballSpeedUpStep = 5.0f;
 	public int maxGameRounds = 3;
+	public int playerLifeDecStep = 10;
 
 
 	private Transform projectile;
 	
 	private float ballSpeed;
 	private int gameRound = 1;
-	private int player1Score = 0;
-	private int player2Score = 0;
+	private int player1Score = 100;
+	private int player2Score = 100;
 
 	void Start() {
 		player1.SetInputAxis("HorizontalP1", "VerticalP1", "ShootP1", "BlockP1");
@@ -45,26 +46,27 @@ public class Game : MonoBehaviour {
 
 			switch ((int)timeLeft) {
 			case 5:
-				canvas.transform.FindChild ("Round_" + gameRound).gameObject.SetActive (true);
+				canvas.transform.FindChild ("Round").GetComponent<Text> ().text = "Round " + gameRound;
+				canvas.transform.FindChild ("Round").gameObject.SetActive (true);
 				break;
 			case 4:
-				canvas.transform.FindChild ("Round_" + gameRound).gameObject.SetActive (false);
+				canvas.transform.FindChild ("Round").gameObject.SetActive (false);
 				break;
 			case 3:
-				canvas.transform.FindChild ("2").gameObject.SetActive (true);
+				canvas.transform.FindChild ("Count_Down").GetComponent<Text> ().text = "2";
+				canvas.transform.FindChild ("Count_Down").gameObject.SetActive (true);
 				break;
 			case 2:
-				canvas.transform.FindChild ("2").gameObject.SetActive (false);
-				canvas.transform.FindChild ("1").gameObject.SetActive (true);
+				canvas.transform.FindChild ("Count_Down").GetComponent<Text> ().text = "1";
 				break;
 			case 1:
-				canvas.transform.FindChild ("1").gameObject.SetActive (false);
+				canvas.transform.FindChild ("Count_Down").gameObject.SetActive (false);
 				canvas.transform.FindChild ("FIGHT").gameObject.SetActive (true);
 				break;
 			case 0:
 				canvas.transform.FindChild ("FIGHT").gameObject.SetActive (false);
-				canvas.transform.FindChild ("Player_1_Score").gameObject.SetActive (true);
-				canvas.transform.FindChild ("Player_2_Score").gameObject.SetActive (true);
+				canvas.transform.FindChild ("Player_1_Life").gameObject.SetActive (true);
+				canvas.transform.FindChild ("Player_2_Life").gameObject.SetActive (true);
 				
 				player1.enabled = true;
 				player2.enabled = true;
@@ -80,8 +82,8 @@ public class Game : MonoBehaviour {
 
 	void LateUpdate() {
 		Debug.Log ("P1: " + player1Score + ". P2: " + player2Score);
-		canvas.transform.FindChild ("Player_1_Score").GetComponent<Text> ().text = "" + player1Score;
-		canvas.transform.FindChild ("Player_2_Score").GetComponent<Text> ().text = "" + player2Score;
+		canvas.transform.FindChild ("Player_1_Life").GetComponent<Text> ().text = "" + player1Score;
+		canvas.transform.FindChild ("Player_2_Life").GetComponent<Text> ().text = "" + player2Score;
 	}
 	
 	public void SetTurn(bool turn) {
@@ -106,6 +108,39 @@ public class Game : MonoBehaviour {
 	public float GetBallSpeed() { return ballSpeed; }
 	public void ResetBallSpeed() { ballSpeed = minBallSpeed; }
 
-	public void Player1Scored() { ++player1Score; }
-	public void Player2Scored() { ++player2Score; }
+	public void Player1Scored() {
+		if (player1Score > 0) {
+			player1Score -= playerLifeDecStep;
+		}
+		
+		if (player1Score <= 0) {
+			player1Score = 0;
+			
+			canvas.transform.FindChild ("Player_Win").GetComponent<Text> ().text = "Player 2 Wins";
+			canvas.transform.FindChild ("Player_Win").gameObject.SetActive (true);
+			
+			player1.enabled = false;
+			player2.enabled = false;
+
+			++gameRound;
+		}
+	}
+
+	public void Player2Scored() {
+		if (player2Score > 0) {
+			player2Score -= playerLifeDecStep;
+		}
+		
+		if (player2Score <= 0) {
+			player2Score = 0;
+			
+			canvas.transform.FindChild ("Player_Win").GetComponent<Text> ().text = "Player 1 Wins";
+			canvas.transform.FindChild ("Player_Win").gameObject.SetActive (true);
+			
+			player1.enabled = false;
+			player2.enabled = false;
+			
+			++gameRound;
+		}
+	}
 }

b9dd2fa8d0e7d7dd73c695c3163b0318c8f843c4
Christoph-M <christoph.marschott@gmx.net>
2015-12-03 20:29:20 +0100

    Win/Lose state implemented + wall bounce now does damage

--- PongKlohn/Assets/Scripts/Ball.cs
+++ PongKlohn/Assets/Scripts/Ball.cs
@@ -26,6 +26,8 @@ public class Ball : MonoBehaviour {
 		linearRotationScript = GameObject.FindObjectOfType (typeof(LinearRotation)) as LinearRotation;
 		sinCosRotationScript = GameObject.FindObjectOfType (typeof(SinCosRotation)) as SinCosRotation;
 
+		this.name = "Projectile";
+
 		gameScript.SetProjectileTransform (this.transform);
 	}
 
@@ -34,6 +36,11 @@ public class Ball : MonoBehaviour {
 		if (sinCosMove) sinCosMovementScript.Update_ ();
 		if (linearRotation) linearRotationScript.Update_ ();
 		if (sinCosRotation) sinCosRotationScript.Update_ ();
+	}
+
+	float timeElapsed = 0.0f;
+	void LateUpdate() {
+		timeElapsed += Time.deltaTime;
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
@@ -77,10 +84,13 @@ public class Ball : MonoBehaviour {
 	}
 
 	private void Goal(GameObject other) {
+		Debug.Log ("Goal. Time: " + timeElapsed);
+		timeElapsed = 0.0f;
+
 		if (other.name == "Goal_Red") {
-			gameScript.Player2Scored ();
+			gameScript.Player2Scored (false);
 		} else {
-			gameScript.Player1Scored ();
+			gameScript.Player1Scored (false);
 		}
 
 		this.SetTurn (other.name);
@@ -90,6 +100,9 @@ public class Ball : MonoBehaviour {
 	}
 
 	private void Catch(GameObject other) {
+		Debug.Log ("Catched. Time: " + timeElapsed);
+		timeElapsed = 0.0f;
+
 		this.SetTurn (other.transform.parent.name);
 		
 		this.DestroyBall ();
@@ -97,6 +110,15 @@ public class Ball : MonoBehaviour {
 	}
 
 	private void Bounce(GameObject other) {
+		Debug.Log ("Bounced. Time: " + timeElapsed);
+		timeElapsed = 0.0f;
+
+		if (this.tag == "RedBall") {
+			gameScript.Player1Scored (true);
+		} else {
+			gameScript.Player2Scored (true);
+		}
+
 		float distance = this.transform.right.magnitude;
 		Vector2 forward = this.transform.right / distance;
 
@@ -113,6 +135,9 @@ public class Ball : MonoBehaviour {
 	}
 
 	private void Block(GameObject other) {
+		Debug.Log ("Blocked. Time: " + timeElapsed);
+		timeElapsed = 0.0f;
+
 		Vector2 playerDirection = other.transform.parent.position - this.transform.position;
 		
 		RaycastHit2D hit = Physics2D.Raycast(this.transform.position, playerDirection);

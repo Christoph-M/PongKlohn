d153aef1e451bb58e0291fc31a5769b75eafa407
Christoph-M <christoph.marschott@gmx.net>
2015-12-17 13:14:57 +0100

    implemented menu + battle music + ball effect sound

---
+++ PongKlohn/Assets/Scripts/AudioLoop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioLoop : MonoBehaviour {
+	public AudioSource monkIntroAudio;
+	public AudioSource monkLoopAudio;
+	
+	void LateUpdate () {
+		if (!monkIntroAudio.isPlaying && !monkLoopAudio.enabled) {
+			monkIntroAudio.enabled = false;
+			monkLoopAudio.enabled = true;
+		}
+	}
+}

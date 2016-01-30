7492a5168ff457e6ba42f6fe04a80cb395685f45
Christoph-M <christoph.marschott@gmx.net>
2016-01-30 18:28:15 +0100

    compilable save

--- PongKlohn/Assets/Scripts/Master/SceneHandler.cs
+++ PongKlohn/Assets/Scripts/Master/SceneHandler.cs
@@ -1,24 +1,84 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
-public class SceneHandler {
-	private List<SceneState> startState;
-	private List<SceneState> matchState;
-	private List<SceneState> campaignState;
-	private List<SceneState> tournamentState;
+public class SceneHandler 
+{
 
-	public SceneHandler() {
-		startState = new List<SceneState> { new SceneState (1, 0),
-											new SceneState (1, 1) };
-
-		matchState = new List<SceneState> { new SceneState (1, 2, -1, -1, 0, 1),
-											new SceneState (2, 0) };
-
-		campaignState = new List<SceneState> { new SceneState (0),
-											   new SceneState (1) };
-
-		tournamentState = new List<SceneState> { new SceneState (0),
-												 new SceneState (1) };
+	public void LoadMainMenue()
+	{
+//		SceneManager.LoadScene (Scenes.GameScene, LoadSceneMode.Additive);
+		//StartCoroutine (SetActiveScene (scene));
+//		SceneManager.LoadScene (Scenes.UIScene, LoadSceneMode.Single);
+		//StartCoroutine (SetActiveScene (scene));
+		//SetActiveUI(UIs.MainUI);??
+		//return UIs.MainUI;??	
 	}
+	
+	public void LoadMatchSelection()
+	{
+//		SceneManager.LoadScene (Scenes.UIScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(List Players);
+		//SetActiveUI(UIs.MatchCharUI);??
+		//return UIs.MatchCharUI;??
+	}
+	
+	public void LoadMacht(int player1 , int player2, int inputp1 , int inputp2)
+	{
+//		SceneManager.LoadScene (Scenes.GameScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(player1 , Player2 ,kristals1 ,kristal2 ,input1 ,input2);
+		//SetActiveUI(UIs.MachtUI);??
+		//return UIs.MachtUI;??	
+	}
+	
+	public void LoadStorryModeCharacterSelection(int Player1, int input, int kristall)
+	{
+//		SceneManager.LoadScene (Scenes.UIScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(player1 , Player2);
+		//SetActiveUI(UIs.SinglePlayerCharUI);??
+		//return UIs.SinglePlayerCharUI;??	
+	}
+	
+	public void LoadStorryModeMap()
+	{
+//		SceneManager.LoadScene (Scenes.UIScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(player1,Kristal1);
+		//SetActiveUI(UIs.SinglePlayerMapUI);??
+		//return UIs.SinglePlayerMapUI;??	
+	}
+	
+	public void LoadStorryModeMatch(int Player1, int Player2)
+	{
+//		SceneManager.LoadScene (Scenes.GameScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(player1 , Player2 ,kristals1 ,kristal2 ,input1 ,input2);
+		//SetActiveUI(UIs.SinglePlayerUI);??
+		//return UIs.SinglePlayerUI;??
+	}
+	
+	public void LoadStorryModeGameOver(int Player1, int Player2)
+	{
+//		SceneManager.LoadScene (Scenes.GameScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(player1 , Player2);
+		//SetActiveUI(UIs.SinglePlayerGameOverUI);??
+		//return UIs.SinglePlayerGameOverUI;??
+	}
+	
+	public void LoadMatchGameOver(int Player1, int Player2)
+	{
+//		SceneManager.LoadScene (Scenes.GameScene, LoadSceneMode.Single);
+//		Tools.LoadMachtElements(player1 , Player2 );
+		//SetActiveUI(UIs.MachtGameOverUI);??
+		//return UIs.MachtGameOverUI;??
+	}
+	
+	enum UIs
+	{
+		StartUI ,MainUI ,SinglePlayerCharUI ,SinglePlayerUI ,SinglePlayerMapUI ,SinglePlayerGameOverUI ,MatchCharUI,MachtUI ,MachtGameOverUI
+	};
+	
+	enum Scenes
+	{
+		MasterScene,UIScene,GameScene
+	};
 }

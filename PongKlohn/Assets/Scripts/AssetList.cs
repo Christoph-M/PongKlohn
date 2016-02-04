c236ca7609b76dcf85bb650287c538269fb0b432
DrBanane <albrecht.kiesewetter@gmx.de>
2016-02-04 17:34:05 +0100

    merge

---
+++ PongKlohn/Assets/Scripts/AssetList.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssetList : MonoBehaviour 
+{
+    public List<GameObject> BlockEffecte;
+    [Space(10)]
+    public List<GameObject> BlockSchieldEffecte;
+    [Space(10)]
+    public List<GameObject> BlockBounceEffecte;
+    [Space(10)]
+    public List<GameObject> WallBounceEffecte;
+    
+    void Start()
+    {
+
+    }
+}
